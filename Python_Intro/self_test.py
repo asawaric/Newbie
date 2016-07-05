0961e7d543ec024738d0c1072c410a50ec4f1cec
Asawari <asawari2688@gmail.com>
2016-07-05 17:16:45 +0800

    added new submodule

--- Python_Intro/self_test.py
+++ Python_Intro/self_test.py
@@ -1,5 +1,8 @@
 # To run this file enter on the command line:
 # python self_test.py
 from SelfCheck import DataStructures
+from SelfCheck import Data_strucutres2
 
 DataStructures.test.run_all_quizzes()
+
+Data_structures2.test.run_all_quizzes()
