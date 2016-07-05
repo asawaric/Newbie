df642174f1684a152718d0063660f3a865e8a668
Asawari <asawari2688@gmail.com>
2016-07-05 17:20:18 +0800

    corrected typo

--- Python_Intro/self_test.py
+++ Python_Intro/self_test.py
@@ -1,7 +1,7 @@
 # To run this file enter on the command line:
 # python self_test.py
 from SelfCheck import DataStructures
-from SelfCheck import Data_strucutres2
+from SelfCheck import Data_structures2
 
 DataStructures.test.run_all_quizzes()
 
