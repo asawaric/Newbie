4e63f334e843823cda06d29b7f458d8640e06ceb
Asawari Choudhari <asawari@kahuna.cbis.nus.edu.sg>
2016-08-05 10:14:06 +0800

    modified Black_box_new

--- Python_Intro/Teaser/Black_box_new.py
+++ Python_Intro/Teaser/Black_box_new.py
@@ -1,11 +1,11 @@
 import random
-def Black_box(n,m):
-    lst1 = [list((random.choice([1,2,3,4,5,6])) for i in range(n)) for i in range(m)]
+def Black_box(dice,throws):
+    lst1 = [list((random.choice([1,2,3,4,5,6])) for i in range(throws)) for i in range(dice)]
     lst2 = [sum(i) for i in lst1]
     dict1 = {v:0 for v in set(lst2)}
     for v in lst2:
         dict1[v] += 1
-    probability = {v:float(dict1[v])/m for v in dict1}
+    probability = {v:float(dict1[v])/throws for v in dict1}
     print probability
 Black_box(1000, 1000)
 
