c78ea89f80d8f464edaa998e0b8046dcedc6dfee
Sam <samuel.day-weiss@yale.ed>
2016-08-10 10:46:26 +0800

    Final Black Box File

--- Python_Intro/Teaser/Black_box_new.py
+++ Python_Intro/Teaser/Black_box_new.py
@@ -1,11 +1,22 @@
 import random
+import math
 def Black_box(dice,throws):
-    lst1 = [list((random.choice([1,2,3,4,5,6])) for i in range(throws)) for i in range(dice)]
+    lst1 = [[random.randint(1,6) for i in range(dice)] for i in range(throws)]
     lst2 = [sum(i) for i in lst1]
     dict1 = {v:0 for v in set(lst2)}
     for v in lst2:
         dict1[v] += 1
     probability = {v:float(dict1[v])/throws for v in dict1}
-    print probability
-Black_box(1000, 1000)
+    return probability
 
+
+def dice_finder(n, m):
+    dict3 = {} 
+    for i in range(int(math.ceil((n/6))),(n+1)):
+        dict2 = Black_box(i, m)
+        if n in dict2.keys():
+            dict3[i] = dict2[n]
+    
+    print dict3
+
+dice_finder(51,10000)
