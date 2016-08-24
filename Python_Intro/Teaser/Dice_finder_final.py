01d8d2a70a3490df08e6c37059afc18bcacda1a3
Asawari Choudhari <asawari@kane.cbis.nus.edu.sg>
2016-08-24 09:16:01 +0800

    recommit

--- Python_Intro/Teaser/Dice_finder_final.py
+++ Python_Intro/Teaser/Dice_finder_final.py
@@ -1,5 +1,7 @@
 import random
 import math
+import matplotlib.pyplot as plt
+import operator
 def Black_box(dice,throws):
     #lst1 = [[(random.choice([1,2,3,4,5,6])) for i in range(dice)] for i in range(throws)]
     lst1 = [[random.randint(1,6) for i in range(dice)] for i in range(throws)]
@@ -17,7 +19,17 @@ def dice_finder(n, m=1000):
         dict2 = Black_box(i, m)
         if n in dict2.keys():
             dict3[i] = dict2[n]
-    print dict3        
+    print dict3
+#def normalized_prob(x, y):
+    #x = sorted_dict3.keys()
+    #y = sorted_dict3.values()
+    #plt.plot(x, y)
+    #plt.xlabel('No. of Dice')
+    #plt.ylabel('P(Sum)')
+    #plt.show()
+    plt.plot(*zip(*sorted(dict3.items(), key=lambda x:x[0])))
+    
+    plt.show()
+dice_finder(91, 1000)
 
-dice_finder(51, 10000)
 #Both versions of list1 works in this code.
